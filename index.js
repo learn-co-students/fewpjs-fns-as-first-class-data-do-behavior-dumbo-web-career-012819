46d5850680c96634069d2641a1679a9db8d0e718
Angelo Spampinato <spampinatodesign@gmail.com>
2019-03-11 17:34:31 -0400

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,20 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time) {
+  if (time.substring(0,2) > 17)  {
+    return "Good Evening";
+  } else if (time.substring(0,2) > 12 && time.substring(0,2 < 17)) {
+    return "Good Afternoon";
+  } else {
+    return "Good Morning";
+  }
+}
+(time.substring(0,2) < 12)
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(string) {
+  document.getElementById('greeting').innerHTML = `<h1>${string}</h1>`;
+}
