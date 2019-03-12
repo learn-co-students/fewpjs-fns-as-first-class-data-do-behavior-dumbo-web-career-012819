b7339271f874208de90548cf35e388ed6993aa1a
Nate <github email address>
2019-03-12 11:30:18 -0400

    tests passed

--- index.js
+++ index.js
@@ -8,4 +8,22 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet (time) {
+  let greetingTime = parseInt(time.split(":"));
+
+  if (greetingTime < 12) {
+    return "Good Morning";
+  } else if (greetingTime < 17) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening"
+  }
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage (greeting) {
+    let greetingNode = document.getElementById('greeting');
+    greetingNode.innerHTML = greeting;
+}
