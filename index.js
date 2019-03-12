0c02be6e31b3249d57cc3d02782d1e670c550961
github username <github email address>
2019-03-12 13:27:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 /* Given Code, don't edit */
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+    const timeString = document.getElementById('time').value
+    displayMessage(greet(timeString))
+    greet(timeString)
 }
 
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString) {
+    const timeSplit = timeString.split(":")
+    const timeJoin = timeSplit.join("")
+    const timeParse = parseInt(timeJoin)
+    if (timeParse < 1200) {
+        return `Good Morning`;
+    }
+    else if (timeParse < 1700 && timeParse > 1200) {
+        return `Good Afternoon`;
+    }
+    else if  (timeParse > 1700) {
+        return `Good Evening`;
+    }
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(string) {
+    const header = document.querySelector("h1")
+    header.innerHTML = string
+
+}
