2b357421693a7cb834cff392eac14e580f74d3d0
Krystle Justine <KrystleJustine@gmail.com>
2019-03-13 13:00:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 /* Given Code, don't edit */
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
+  const timeString = document.getElementById('time').value;
   displayMessage(greet(timeString))
 }
+
+function greet(time) {
+  if (parseInt (time, 10) < 12) return "Good Morning";
+  else if (parseInt(time, 10) < 17) return "Good Afternoon";
+  else return "Good Evening"
+}
+
+function displayMessage(display){
+  document.getElementById("greeting").textContent = display
+}
+
 
 /* End Given Code, don't edit above here...*/
 
